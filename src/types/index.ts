345459fc382268d6eb287800a0bac017988667a4
Anastasiia-Startseva <yui67.9@yandex.ru>
2026-06-30 12:05:48 +0300

    chore: add typescript interfaces for models

---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+export interface Product {
+  id: string;
+  name: string;
+  price: number;
+  stock: number;
+}
+
+export interface Order {
+  id: string;
+  customerName: string;
+  total: number;
+  status: 'Ожидает' | 'Выполнен';
+}
+
+export interface Customer {
+  id: string;
+  name: string;
+  email: string;
+}
